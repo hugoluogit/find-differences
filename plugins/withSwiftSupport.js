e30ad04ee07e74d44d7babc8653670c4bc708753
hugoluogit <hugoluogit@users.noreply.github.com>
2026-06-19 17:45:08 +0800

    Fix ITMS-90863: patch xcode library to quote build setting keys with [sdk=] conditional syntax

--- plugins/withSwiftSupport.js
+++ plugins/withSwiftSupport.js
@@ -5,15 +5,25 @@ const fs = require('fs');
 module.exports = function withSwiftSupport(config) {
   return withXcodeProject(config, (config) => {
     const proj = config.modResults;
-    const swiftPath = path.join(config.modRequest.platformProjectRoot, 'app', 'Empty.swift');
 
+    // Add empty Swift file to ensure Swift runtime is embedded
+    const swiftPath = path.join(config.modRequest.platformProjectRoot, 'app', 'Empty.swift');
     if (!fs.existsSync(swiftPath)) {
       fs.writeFileSync(swiftPath, 'import Foundation\n', 'utf8');
     }
-
     const target = proj.getTarget('app');
     if (target) {
       proj.addSourceFile('app/Empty.swift', target, {});
+    }
+
+    // Set EXCLUDED_ARCHS for macOS to suppress ITMS-90863 warnings
+    // The xcode library is patched (via patch-package) to quote keys with [sdk=]
+    const configs = proj.pbxXCBuildConfigurationSection();
+    for (const key in configs) {
+      const bs = configs[key]?.buildSettings;
+      if (bs) {
+        bs["EXCLUDED_ARCHS[sdk=macosx*]"] = "arm64";
+      }
     }
 
     return config;
